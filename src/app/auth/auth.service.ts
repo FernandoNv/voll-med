1af4d0c2a9bf93d7f11e05996169325ef587bda5
Fernando Vieira <fernandonvieira47@hotmail.com>
2023-03-05 11:04:37 -0300

    atualiza arquivo de environment

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { IUser, IUserSignIn } from './model/user.model';
+import { IUserSignIn } from './model/user.model';
 import { UserService } from './user.service';
 
 const API = environment.apiUrl;
